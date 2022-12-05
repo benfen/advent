ccf3ba28ac084c8fd6d34df044f3ff8c6fc798aa
matt-brooks <mbrook16@masonlive.gmu.edu>
2022-12-05 15:48:26 -0700

    done with day 4

---
+++ 2022/day_04/c_sharp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace c_sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+			string[] pairStrings = System.IO.File.ReadAllLines("./input.txt");
+            int pairsLength = pairStrings.Length;
+            int solutionOne = 0;
+            int solutionTwo = 0;
+
+            for (int i = 0; i < pairsLength; i++) {
+                string[] pairs = pairStrings[i].Split(',');
+                string[] firstElfRange = pairs[0].Split('-');
+                string[] secondElfRange = pairs[1].Split('-');
+                
+                if (
+                    int.Parse(firstElfRange[0]) <= int.Parse(secondElfRange[0]) && int.Parse(firstElfRange[1]) >= int.Parse(secondElfRange[1])
+                    || int.Parse(secondElfRange[0]) <= int.Parse(firstElfRange[0]) && int.Parse(secondElfRange[1]) >= int.Parse(firstElfRange[1])
+                ) {
+                    solutionOne++;
+                }
+
+                if (
+                    int.Parse(firstElfRange[0]) <= int.Parse(secondElfRange[1]) && int.Parse(firstElfRange[0]) >= int.Parse(secondElfRange[0])
+                    || int.Parse(secondElfRange[0]) <= int.Parse(firstElfRange[1]) && int.Parse(secondElfRange[0]) >= int.Parse(firstElfRange[0])
+                ) {
+                    solutionTwo++;
+                }
+            }
+
+
+            System.Console.WriteLine(solutionOne);
+            System.Console.WriteLine(solutionTwo);
+        }
+    }
+}
