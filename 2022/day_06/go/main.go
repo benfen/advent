dada804e1a2e41e89822fbb6f85cd448ae755036
Chris Berger <flippinberger@gmail.com>
2022-12-06 10:53:53 -0700

    Go: Use 1 loop instead of 2

--- 2022/day_06/go/main.go
+++ 2022/day_06/go/main.go
@@ -20,26 +20,32 @@ func main() {
 	input := scanner.Text()
 
 	packetSize := 4
+	msgSize := 14
 
 	s := input[:packetSize]
+	s2 := input[:msgSize]
+	packetFound := false
+	msgFound := false
 
 	packetMark := -1
+	msgMark := -1
 	for i := packetSize + 1; i < len(input); i++ {
-		s = input[i-packetSize : i]
-		if checkWindow(s, packetSize) {
-			packetMark = i
+		if packetFound && msgFound {
 			break
 		}
-	}
-
-	msgSize := 14
-	msgMark := -1
-	s = input[:msgSize]
-	for i := msgSize + 1; i < len(input); i++ {
-		s = input[i-msgSize : i]
-		if checkWindow(s, msgSize) {
-			msgMark = i
-			break
+		if !packetFound {
+			s = input[i-packetSize : i]
+			if checkWindow(s, packetSize) {
+				packetMark = i
+				packetFound = true
+			}
+		}
+		if !msgFound && i > msgSize {
+			s2 = input[i-msgSize : i]
+			if checkWindow(s2, msgSize) {
+				msgMark = i
+				msgFound = true
+			}
 		}
 	}
 
